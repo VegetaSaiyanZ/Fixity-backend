41c791f51c61a5256cc5e0941b9ecc260781cab0
Yaelgos <214190522@idf.il>
2026-04-04 16:03:23 +0300

    added tenplate server files

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import routes from './routes';
+
+const app: Application = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+// Routes
+app.use('/api', routes);
+
+// Base route
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Welcome to the Express Backend API!' });
+});
+
+// 404 Handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ error: 'Endpoint not found' });
+});
+
+// Global Error Handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+export default app;
